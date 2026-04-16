8d8918279e37d03048629fc4432901b92e7bd0fc
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-04-16 17:00:04 -0300

    Inicializando meu primeiro projeto em JAVA

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { // método principal que executa o código
+		// Nossa Lógica aqui
+		
+		System.out.print("Hello World"); // Identico ao escreva()
+		
+	}
+
+}
